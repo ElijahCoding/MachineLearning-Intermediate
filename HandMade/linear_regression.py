67ff0fab05d63a846a46cd95524344734af7d48d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-12 22:13:58 +0300

    linear regression

--- HandMade/linear_regression.py
+++ HandMade/linear_regression.py
@@ -1,14 +1,29 @@
-
+import numpy as np
 
 class LinearRegression:
-    def __init__(self, lr=0.001, n_iters=1000):
-        self.lr = lr
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.learning_rate = learning_rate
         self.n_iters = n_iters
         self.weights = None
-        self.biases = None
+        self.bias = None
     
     def fit(self, X, y):
-        pass
-    
+        # init parameters
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + self.bias
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            # update parameters
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
     def predict(self, X):
-        pass
+        y_approximated = np.dot(X, self.weights) + self.bias
+        return y_approximated
